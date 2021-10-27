81fc807094b646e83313f038504b35ca53555b2d
Abderrahim Jami <56297253+abderrahimjami1700081@users.noreply.github.com>
2021-10-27 15:13:03 +0100

    Set up basic application and entrypoint.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For use by Hazel Applications 
+
+#include "Hazel/Application.h"
+
+// --------------- Entry point -----------
+
+#include "Hazel/Entrytpoint.h"
+
+// --------------- -----------------------
